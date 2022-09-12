d00e5a6437d8734d7cda96e13a1557880b612137
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-09-12 20:22:26 +0300

    Задание готово. Доработал с помощью @RequestMapping

--- src/main/java/pro/sky/profjd_10_0_hw2_3/controller/CalcController.java
+++ src/main/java/pro/sky/profjd_10_0_hw2_3/controller/CalcController.java
@@ -1,11 +1,14 @@
 package pro.sky.profjd_10_0_hw2_3.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.profjd_10_0_hw2_3.service.CalcService;
 
 @RestController
+
+@RequestMapping("/calculator")
 public class CalcController {
     private final CalcService calcService;
 
@@ -14,32 +17,27 @@ public class CalcController {
     }
 
     @GetMapping
-    public String showHelloWorld() {
-        return calcService.hello();
-    }
-
-    @GetMapping("/calculator")
     private String calculator() {
         return calcService.greetings();
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     private String calculateSum(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " + " + num2 + " = " + calcService.calculateSum(num1, num2);
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
 
     private String calculateSubtract(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " - " + num2 + " = " + calcService.calculateSubtract(num1, num2);
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     private String calculateProduct(@RequestParam int num1, @RequestParam int num2) {
         return num1 + " * " + num2 + " = " + calcService.calculateProduct(num1, num2);
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     private String calculateDivision(@RequestParam int num1, @RequestParam int num2) {
         if (num2 == 0) {
             return "Ошибка! На 0 не делим, введите другое число №2";
