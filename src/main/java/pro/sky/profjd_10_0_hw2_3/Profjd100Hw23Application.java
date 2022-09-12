685498e8c305c7dbbfd45ae9e9f9ff85c3c80ffb
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-09-12 16:28:57 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/profjd_10_0_hw2_3/Profjd100Hw23Application.java
@@ -0,0 +1,13 @@
+package pro.sky.profjd_10_0_hw2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Profjd100Hw23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Profjd100Hw23Application.class, args);
+	}
+
+}
